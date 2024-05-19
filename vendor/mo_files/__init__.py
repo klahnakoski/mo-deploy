eecbd3c53f9788e86de5fc7583c36b86a5d409ad
Kyle Lahnakoski <kyle@lahnakoski.com>
2024-05-19 17:10:30 -0400

    fixes

--- vendor/mo_files/__init__.py
+++ vendor/mo_files/__init__.py
@@ -20,6 +20,7 @@ from mo_dots import Null, coalesce, get_module, is_list, to_data
 from mo_files import mimetype
 from mo_files.url import URL
 from mo_future import text, is_text, ConfigParser, StringIO
+from mo_json import json2value
 from mo_logs import Except, Log
 from mo_logs.exceptions import get_stacktrace
 from mo_math import randoms
@@ -234,11 +235,8 @@ class File(object):
                 yield line.decode(encoding).rstrip()
 
     def read_json(self, encoding="utf8", flexible=True, leaves=True):
-        from mo_json import json2value
-
         content = self.read(encoding=encoding)
-        value = json2value(content, flexible=flexible)
-        return get_module("mo_json_config").expand(value, "file://" + self.abs_path)
+        return json2value(content, flexible=flexible)
 
     def is_directory(self):
         return os.path.isdir(self._filename)
